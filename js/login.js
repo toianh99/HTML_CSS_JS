435a0e5d57fb9cb40c2ef20c5c154de1f72f5442
toianh99 <toileanh84@gmail.com>
2020-09-13 09:52:56 -0700

    //add js cho form login

---
+++ js/login.js
@@ -0,0 +1,16 @@
+function Login(){
+    var x=document.getElementById('username').value;
+    var y =document.getElementById('password').value;
+    if(x==='admin'&&y==='admin'){
+       
+        alert("Đăng nhập thành công");
+    }
+    else
+    document.getElementById('form_login').action='login.html';
+}
+// function getUserName(){
+//     x=document.getElementById('username').value;
+// }
+// function getPassword(){
+//     y =document.getElementById('password').value;
+// }
